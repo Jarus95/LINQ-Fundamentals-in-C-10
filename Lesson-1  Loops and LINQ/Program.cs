f95e85e30b53d6fcd0b9b77c7b9a18c5184892fb
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-06-22 12:29:05 +0500

    lesson 1

---
+++ Lesson-1  Loops and LINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿//SELECT * FROM Products WHERE ListPrice > 1000
+
+using DataLayer.Entities;
+using LINQSamples;
+
+
+ViewModelBase viewModelBase = new ViewModelBase();
+
+//loop
+List<Product> list_loop = new();
+List<Product> products_loop = viewModelBase.GetProducts();
+foreach (Product product in products_loop)
+{
+    if (product.ListPrice > 1000)
+    {
+        list_loop.Add(product);
+    }
+}
+
+//LINQ
+
+
+List<Product> products = viewModelBase.GetProducts();
+var list = (from prod in products where prod.ListPrice > 1000 select prod).ToList();
+Console.WriteLine(list);
+
+
