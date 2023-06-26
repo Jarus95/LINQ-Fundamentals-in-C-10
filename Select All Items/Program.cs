f809ba3208243bd6e137eeafcb944e3e799f8085
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-06-26 09:11:58 +0500

    lesson 2

---
+++ Select All Items/Program.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Entities;
+using LINQSamples;
+
+ViewModelBase viewModelBase = new ViewModelBase();
+List<Product> products = viewModelBase.GetProducts();
+List<Product> listQuery = new();
+List<Product> listMethod = new();
+// Linq query syntax
+listQuery = (from prod in products select prod).ToList();
+//linq method syntax
+listMethod = products.Select(x => x).ToList();
+
+foreach (Product prod in listMethod)
+{
+    Console.WriteLine(prod);
+}
