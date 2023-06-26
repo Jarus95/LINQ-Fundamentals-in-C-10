b4534e3e41ba1e8b9ba3d1bf58ea5422e8b7a96e
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-06-26 09:23:44 +0500

    Get Specific Columns

---
+++ Get Specific Columns/Program.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Entities;
+using LINQSamples;
+
+ViewModelBase viewModelBase = new ViewModelBase();
+List<Product> products = viewModelBase.GetProducts();
+List<Product> listQuery = new();
+List<Product> listMethod = new();
+// Linq query syntax
+listQuery = (from prod in products select new Product
+{
+    ProductID = prod.ProductID,
+    ListPrice = prod.ListPrice
+    
+}).ToList();
+//linq method syntax
+listMethod = products.Select(x => new Product
+{
+    ProductID = x.ProductID,
+    ListPrice = x.ListPrice
+    
+
+}).ToList();
+
+foreach (Product prod in listMethod)
+{
+    Console.WriteLine(prod);
+}
