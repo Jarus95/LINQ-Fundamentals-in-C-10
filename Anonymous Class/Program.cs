806041accb76ef01b20644b6062a352b0f539f63
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-06-26 09:30:35 +0500

    Anonymous Class

---
+++ Anonymous Class/Program.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer.Entities;
+using LINQSamples;
+
+ViewModelBase viewModelBase = new ViewModelBase();
+List<Product> products = viewModelBase.GetProducts();
+
+// Linq query syntax
+var listQuery = (from prod in products
+             select new
+             {
+                 id = prod.ProductID,
+                 name = prod.Name,
+                 color = prod.Color,
+                 size = prod.Size
+             });
+//linq method syntax
+var listMethod = products.Select(x => new 
+{
+    id = x.ProductID,
+    name = x.Name,
+    color = x.Color,
+    size =  x.Size
+
+
+}).ToList();
+
+foreach (var prod in listMethod)
+{
+    Console.WriteLine(prod);
+}
