6cc384234fe7657045896fa11c42adacb21e5997
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-06-26 09:18:01 +0500

    select a single column

---
+++ Select a single column/Program.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Entities;
+using LINQSamples;
+
+ViewModelBase viewModelBase = new ViewModelBase();
+List<Product> products = viewModelBase.GetProducts();
+List<string> listQuery = new();
+List<string> listMethod = new();
+// Linq query syntax
+listQuery = (from prod in products select prod.Name).ToList();
+//linq method syntax
+listMethod = products.Select(x => x.Name).ToList();
+
+foreach (var prod in listMethod)
+{
+    Console.WriteLine(prod);
+}
