51fafc5b8fed7209090a2e71e6ad42a7c9e4c98d
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-06-29 14:25:22 +0500

    Sorting Data in Descending Order

---
+++ Lesson-3 Use LINQ to Order Data/Sorting Data in Descending Order/Program.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Entities;
+using LINQSamples;
+
+ViewModelBase viewModelBase = new ViewModelBase();
+List<Product> products = viewModelBase.GetProducts();
+List<Product> listQuery = new();
+List<Product> listMethod = new();
+// Linq query syntax
+listQuery = (from prod in products orderby prod.Name descending select prod).ToList();
+//linq method syntax
+listMethod = products.OrderByDescending(prod => prod.Name).ToList();
+
+//}).ToList();
+
+foreach (Product prod in listQuery)
+{
+    Console.WriteLine(prod);
+}
