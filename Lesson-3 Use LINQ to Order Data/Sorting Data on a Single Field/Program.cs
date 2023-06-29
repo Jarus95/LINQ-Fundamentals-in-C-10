53de9e9368717053e3f8f19cd0fc03b65b56517e
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-06-29 14:22:10 +0500

    Sorting Data on a Single Field

--- Lesson-3 Use LINQ to Order Data/Sorting Data on a Single Field/Program.cs
+++ Lesson-3 Use LINQ to Order Data/Sorting Data on a Single Field/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataLayer.Entities;
+using LINQSamples;
+
+ViewModelBase viewModelBase = new ViewModelBase();
+List<Product> products = viewModelBase.GetProducts();
+List<Product> listQuery = new();
+List<Product> listMethod = new();
+// Linq query syntax
+listQuery = (from prod in products orderby prod.Name select prod).ToList();
+//linq method syntax
+listMethod = products.OrderBy(prod => prod.Name).ToList();
+
+//}).ToList();
+
+foreach (Product prod in listMethod)
+{
+    Console.WriteLine(prod);
+}
